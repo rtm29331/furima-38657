98208843f68198e18c90d614e1e79e8fd540643d
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-13 15:45:27 +0900

    app/model/user変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :birthday, presence: true
+         with_options presence: true do
+          validates :nickname
 
-
-        
-         
+          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+          validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
+      
+          with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字で入力して下さい." } do
+            validates :last_name
+            validates :first_name
+          end
+      
+          with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい." } do
+            validates :last_name_kana
+            validates :first_name_kana
+          end
+      
+          validates :birthday
+        end
 end
