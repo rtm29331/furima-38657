9dfae19ac0812e89bdb65391ad6c4e89f4bf7898
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-14 14:23:43 +0900

    モデル単体コード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,14 @@ class User < ApplicationRecord
           validates :nickname
 
           PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-          validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
+          validates_format_of :password, with: PASSWORD_REGEX
       
-          with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字で入力して下さい." } do
+          with_options format: { with: /\A[ぁ-んァ-ン一-龥]/} do
             validates :last_name
             validates :first_name
           end
       
-          with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい." } do
+          with_options format: {with: /\A[ァ-ヶー－]+\z/} do
             validates :last_name_kana
             validates :first_name_kana
           end
