57e415b451344f3b7a567b1b42f426fb5d70cc0c
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-14 14:53:50 +0900

    Rubocop調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         with_options presence: true do
-          validates :nickname
+  with_options presence: true do
+    validates :nickname
+  end
 
-          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-          validates_format_of :password, with: PASSWORD_REGEX
-      
-          with_options format: { with: /\A[ぁ-んァ-ン一-龥]/} do
-            validates :last_name
-            validates :first_name
-          end
-      
-          with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-            validates :last_name_kana
-            validates :first_name_kana
-          end
-      
-          validates :birthday
-        end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birthday
 end
