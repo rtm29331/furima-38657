4cec7574559d8442efa2d59a0ca2bc10af3f0223
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-14 22:37:16 +0900

    ログインログアウト商品移動調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
-    # @items = Item
+  #   @items = Item
   end
+
+  def new
+    # @item = Item.new
+  end
+  
+  
+  
+  
+  
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+
+
+
+
+
 end
