ef00ecf1366163575010075bf04075883a5f87b5
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-15 17:27:38 +0900

    active_storageのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,17 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one    :order
 
   has_one_attached :image
+  
+  # active_storageのアソシエーション
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
+  
   
   
   with_options presence: true do
@@ -15,6 +23,6 @@ class Item < ApplicationRecord
     validates :shipping_charge_id
     validates :shipping_area_id
     validates :days_to_ship_id
-    validates :price
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  has_many :orders
   
-    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  
-    with_options presence: true do
-    validates :nickname
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ } do
-      validates :last_name
-      validates :first_name
-    end
+  with_options presence: true do
+  validates :nickname
 
-    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
-      validates :last_name_kana
-      validates :first_name_kana
-    end
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ } do
+    validates :last_name
+    validates :first_name
+  end
 
-    validates :birthday
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birthday
   end
 end
