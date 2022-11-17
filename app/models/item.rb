ac029b37eab9f1dcc828e0028ba3ad9167aca5a8
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-17 19:56:30 +0900

    計算のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,10 +22,11 @@ class Item < ApplicationRecord
     validates :shipping_area_id
     validates :shipping_day_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, presence:true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 }
+    
   end
 
-  with_options numericality: { other_than: 0 } do
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
     validates :shipping_charge_id
