e155bf134e6a92e2e597f6ab846ef6b6100c6c79
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-17 20:06:06 +0900

    0⇨１に変更

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -58,7 +58,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'カテゴリーの情報が「---」だと出品できない' do
-        @item.category_id = 0
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category must be other than 0')
       end
@@ -69,19 +69,19 @@ RSpec.describe Item, type: :model do
       end
       
       it '配送料の負担の情報が「---」だと出品できない' do
-        @item.shipping_charge_id = 0
+        @item.shipping_charge_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping charge must be other than 0')
       end
       
       it '発送元の地域の情報が「---」だと出品できない' do
-        @item.shipping_area_id = 0
+        @item.shipping_area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping area must be other than 0')
       end
       
       it '発送までの日数の情報が「---」だと出品できない' do
-        @item.shipping_day_id = 0
+        @item.shipping_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping day must be other than 0')
       end
