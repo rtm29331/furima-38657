2735e677dfdbea27466e5e0b0d169a06054b3bc3
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-17 19:42:02 +0900

    Math.floor変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  # before_action :set_item, only: [:show, :edit]
 
-  def index
-    @items = Item
-  end
+  # def index
+  #   # @items = Item
+  # end
 
   def new
     @item = Item.new
--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,18 @@
-window.addEventListener('load', () => {
+window.addEventListener("load",()=>{
+
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    const ProfitDom = document.getElementById("profit");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-    ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+    console.log(inputValue);
+
+    const addTaxDom = document.getElementById("add-tax-price")
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+     console.log(addTaxDom);
+
+      const profitNumber = document.getElementById("profit")
+    const value_result = inputValue * 0.1
+    console.log(value_result)
+    profitNumber.innerHTML = (Math.floor(inputValue - value_result));
+      console.log(profitNumber);
   })
-});
+})
