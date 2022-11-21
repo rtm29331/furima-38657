7a23e3fd65e99957d99c0867d6e4579050436758
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-21 16:01:25 +0900

    調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  
-  
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -26,21 +25,20 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless @item.user == current_user
-      redirect_to root_path
-    end
+    return if @item.user_id == current_user.id
+
+    redirect_to root_path
   end
 
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to item_path
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
   end
-  
-  
+
   # def destroy
   #   @item = Item.find(params[:id])
   #   return unless @item.destroy
@@ -52,6 +50,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :shipping_charge_id, :shipping_area_id,
                                  :shipping_day_id, :price).merge(user_id: current_user.id)
   end
- 
-
 end
