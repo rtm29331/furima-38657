cc233d9a010ced1a2612be4ba2e74066f1c32300
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-21 18:47:27 +0900

    色々やり直し。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :index, :show, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all.order('created_at DESC')
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -19,33 +19,30 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
-
   def edit
-    if @item.user_id == current_user.id
-      redirect_to root_path
-    else
-      render :edit
-    end
+    return if @item.user_id == current_user.id
+
+    redirect_to root_path
   end
 
   def update
-    if @item.update(item_params)
-      redirect_to item_path(@item.id)
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
     else
-      render :edit
+      render 'edit'
     end
   end
 
-  def destroy
-    if @item.user_id == current_user.id
-      @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+  def show
   end
+
+  def destroy
+    @item.destroy if @item.user_id == current_user.id
+    redirect_to root_path
+  end
+
+  private
 
   def item_params
     params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :shipping_charge_id, :shipping_area_id,
