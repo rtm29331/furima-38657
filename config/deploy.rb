926c6756b85aa8d1982241c0985b6d9fea0c5aae
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-11-29 22:09:46 +0900

    kesita

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '(3.17.1)'
 set :application, 'furima-38657'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:rtm29331/furima-38657].git'
+set :repo_url,  'git@github.com:rtm29331/furima-38657.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
